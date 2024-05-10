45b1ca676ecbf86acc5f785841450a98a293bc78
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-05-10 12:07:28 +0300

    Add files via upload

---
+++ MyWeb/MyWebDataLayer/DataModels/PurchasePrice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyWebDataLayer.DataModels;
+
+public partial class PurchasePrice
+{
+    public Guid Id { get; set; }
+
+    public Guid ItemId { get; set; }
+
+    public DateTime PriceDate { get; set; }
+
+    public decimal Price { get; set; }
+
+    public virtual Item Item { get; set; } = null!;
+}
